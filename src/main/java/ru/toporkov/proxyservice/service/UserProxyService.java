b06e8800d4e14b67e5a377961afb9ae572c3449d
teddy <trololo221@bk.ru>
2024-03-12 21:54:03 +0300

    Добавлены UserProxyService и UserController

--- src/main/java/ru/toporkov/proxyservice/service/UserProxyService.java
+++ src/main/java/ru/toporkov/proxyservice/service/UserProxyService.java
@@ -1,19 +1,33 @@
 package ru.toporkov.proxyservice.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import ru.toporkov.proxyservice.integration.domain.user.User;
 
-import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserProxyService {
 
     private final RestTemplate restTemplate;
-    private final String uri = "https://jsonplaceholder.typicode.com/users";
+    private final String userURI = "https://jsonplaceholder.typicode.com/users";
+    private final ObjectMapper mapper = new ObjectMapper();
 
-    public void findAll() {
-        restTemplate.getForObject(uri, String.class);
+    public List<User> findAll() {
+        var object = restTemplate.getForObject(userURI, Object[].class);
+
+        return object != null ? Arrays.stream(object)
+                .map(obj -> mapper.convertValue(obj, User.class))
+                .collect(Collectors.toList()) : Collections.emptyList();
+    }
+
+    public User findById(Long id) {
+        return restTemplate.getForObject(userURI + "/" + id, User.class);
     }
 }
--- src/main/java/ru/toporkov/proxyservice/web/controller/UserController.java
+++ src/main/java/ru/toporkov/proxyservice/web/controller/UserController.java
@@ -2,9 +2,13 @@ package ru.toporkov.proxyservice.web.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.toporkov.proxyservice.integration.domain.user.User;
 import ru.toporkov.proxyservice.service.UserProxyService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,7 +18,12 @@ public class UserController {
     private final UserProxyService userProxyService;
 
     @GetMapping
-    public void findAll() {
-        userProxyService.findAll();
+    public List<User> findAll() {
+        return userProxyService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable("id") Long id) {
+        return userProxyService.findById(id);
     }
 }
