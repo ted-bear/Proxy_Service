7a5c38e6bb711d8d2ee14e8bbf22621ecc915050
teddy <trololo221@bk.ru>
2024-03-13 22:30:21 +0300

    Исправлен PostController

--- src/main/java/ru/toporkov/proxyservice/web/controller/PostController.java
+++ src/main/java/ru/toporkov/proxyservice/web/controller/PostController.java
@@ -8,14 +8,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.toporkov.proxyservice.integration.service.PostProxyService;
 import ru.toporkov.proxyservice.web.dto.post.PostProxyCreateEditDto;
 import ru.toporkov.proxyservice.web.dto.post.PostProxyReadDto;
 
 import java.util.List;
 
-@Service
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
 public class PostController {
 
     private final PostProxyService postProxyService;
